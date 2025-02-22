58bd13a033e75a9a99db8edcecf76ad813fb8738
ra334 <savcukmihajlo@gmail.com>
2025-02-22 19:13:10 +0200

    add modals eng text

--- public/locales/en/index.ts
+++ public/locales/en/index.ts
@@ -1,9 +1,11 @@
 import filter from './filter.json'
+import apply from './modals/apply.json'
 import notFound from './not-found.json'
 import translation from './translation.json'
 
 export const en = {
 	filter: filter,
 	translation: translation,
+	apply: apply,
 	'not-found': notFound
 } as const
