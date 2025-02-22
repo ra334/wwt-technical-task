09c103c86a2575669d1f10d913f4637995c8ed06
ra334 <savcukmihajlo@gmail.com>
2025-02-22 13:27:23 +0200

    translate main page

--- public/locales/en/index.ts
+++ public/locales/en/index.ts
@@ -1,7 +1,9 @@
 import filter from './filter.json'
 import notFound from './not-found.json'
+import translation from './translation.json'
 
 export const en = {
 	filter: filter,
+	translation: translation,
 	'not-found': notFound
 } as const
